32d693e51bcbe5b7dab54920a16e3dfe90b83d34
theprodigy18 <168449386+theprodigy18@users.noreply.github.com>
2024-11-05 21:15:53 +0700

    Create PesananOnline.js

---
+++ client/src/pages/admin/PesananOnline.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import AdminHeader from '../../components/admin/AdminHeader'
+import Sidebar from '../../components/admin/Sidebar'
+import PesananSection from '../../components/admin/PesananSection'
+
+
+function PesananOnline() 
+{
+    return (
+        <div>
+            <AdminHeader />
+            <Sidebar />
+            <PesananSection />
+        </div>
+    )
+}
+
+export default PesananOnline
